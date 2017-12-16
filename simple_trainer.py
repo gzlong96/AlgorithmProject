aa4a95b42fedb21beb3e2aeff3c7c514b66df8e8
gzlong96 <30570937+gzlong96@users.noreply.github.com>
2017-12-16 15:49:51 +0800

    Update simple_trainer.py

--- simple_trainer.py
+++ simple_trainer.py
@@ -9,10 +9,12 @@ np.random.seed(1234567)
 
 model = Sequential()
 # Dense(64) is a fully-connected layer with 64 hidden units.
-model.add(Dense(64, activation='relu', input_dim=40))
-model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu', input_dim=data_reader.fft_n*4))
+model.add(Dropout(0.4))
 model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.3))
+model.add(Dense(16, activation='relu'))
+model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
 
 adam = Adam(lr=0.01)
@@ -23,6 +25,9 @@ model.compile(loss='binary_crossentropy',
 all_seq, all_label = data_reader.read_all()
 y = all_label[0]
 x = all_seq[0]
+
+acc_history=[]
+
 for i in range(10): #10 rounds
     x_train = []
     y_train = []
@@ -30,11 +35,11 @@ for i in range(10): #10 rounds
     y_test = []
     selects = []
     for j in range(5):
-        num = random.randint(0,25)
+        num = random.randint(0,35)
         while num in selects:
-            num = random.randint(0, 25)
+            num = random.randint(0, 35)
         selects.append(num)
-    for j in range(26):
+    for j in range(36):
         if j in selects:
             x_test.append(x[j])
             y_test.append(y[j])
@@ -46,10 +51,12 @@ for i in range(10): #10 rounds
     x_train = np.array(x_train)
     y_train = np.array(y_train)
     print x_train.shape
-    model.fit(x_train, y_train, epochs=100, batch_size=8,shuffle=True)
+    model.fit(x_train, y_train, epochs=250, batch_size=16,shuffle=True)
     score = model.evaluate(x_test, y_test, batch_size=5)
+    acc_history.append(score[1])
     print "test "+str(i+1)+str(score)
 
-score = model.evaluate(x, y, batch_size=26)
+score = model.evaluate(x, y, batch_size=36)
 print "final test " + str(score)
+print sum(acc_history)/10
 # model.save("/root/merge_model")
